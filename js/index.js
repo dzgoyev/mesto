37faa35fd32d6a57c18884d628b1c02bfa80dfee
dzgoyev <dzgoyev@yandex.ru>
2020-05-31 00:27:53 +0300

    Исправил все баги

--- js/index.js
+++ js/index.js
@@ -1,37 +1,3 @@
-//initial array
-const initialCards = [
-  {
-    name: 'Архыз',
-    link:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link:
-      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
 // Popups
 const popup = document.querySelector('.popup'); //popup profile (other be its elements)
 const popupAdd = document.querySelector('.popup__add'); // popup for adding a card
@@ -51,90 +17,113 @@ const formElementAdd = document.querySelector('.popup__form_add'); // form for a
 // Modifier
 const popupOpened = popup.querySelector('.popup_opened'); //open and close modifier popup
 
+//сюда галлерею
+const container = document.querySelector('.main'); //main container
+const gallery = container.querySelector('.gallery'); //all gallery
+
+//вытащил наружу темплэйт
+const galleryTemplate = document.querySelector('#gallery-template').content;
+
 // Inputs and output fileds profile
-let nameInput = popup.querySelector('.popup__form-item_name'); //name field in the profile registration
-let jobInput = popup.querySelector('.popup__form-item_job'); // name job in the profile registration
-let profileName = document.querySelector('.profile__name'); //name element on the page
-let profileJob = document.querySelector('.profile__job'); //name job jn the page
+const nameInput = popup.querySelector('.popup__form-item_name'); //name field in the profile registration
+const jobInput = popup.querySelector('.popup__form-item_job'); // name job in the profile registration
+const profileName = document.querySelector('.profile__name'); //name element on the page
+const profileJob = document.querySelector('.profile__job'); //name job jn the page
 
 // Inputs and output fields add images
-let container = document.querySelector('.main'); //main container
-let gallery = container.querySelector('.gallery'); //all gallery
-let namePlace = popupAdd.querySelector('.popup__form-item_place'); //name place in the form add
-let linkImage = popupAdd.querySelector('.popup__form-item_link-img'); //link img in the form add
-let galleryItem = container.querySelector('.gallery__item'); //images element on the page
-let galleryName = container.querySelector('.gallery__title'); //place name element on the page
-let like = gallery.querySelector('.gallery__like'); // like for cards
+
+const namePlace = popupAdd.querySelector('.popup__form-item_place'); //name place in the form add
+const linkImage = popupAdd.querySelector('.popup__form-item_link-img'); //link img in the form add
 
 //The function on creating cards
-function addPlace(linkImageValue, namePlaceValue) {
-  const galleryTemplate = document.querySelector('#gallery-template').content;
+
+function createCard(linkImageValue, namePlaceValue) {
   const galleryElement = galleryTemplate.cloneNode(true);
   galleryElement.querySelector('.gallery__item').src = linkImageValue;
   galleryElement.querySelector('.gallery__title').textContent = namePlaceValue;
   galleryElement.querySelector('.gallery__item').alt = namePlaceValue;
+  // --- large image output
+  const cardImage = galleryElement.querySelector('.gallery__item');
+  const popupImageLarge = document.querySelector('.popup__image-src'); // src image
+  const popupImageCaption = document.querySelector('.popup__caption'); //caption for large images
+  cardImage.src = linkImageValue;
+  cardImage.addEventListener('click', function () {
+    popupOpenImg(); //the function adds a modifier for pop-up popup large images
+    popupImageLarge.src = linkImageValue;
+    popupImageLarge.alt = `${namePlaceValue} - увеличенное изображение`;
+    popupImageCaption.textContent = namePlaceValue;
+  });
   //Like
   galleryElement
     .querySelector('.gallery__like')
     .addEventListener('click', function (evt) {
       evt.target.classList.toggle('gallery__like_active');
     });
-  //Add new element to the begining
-  gallery.prepend(galleryElement);
-  const galleryContainer = gallery.querySelector('.gallery__container'); // container for add cards
-  // Trash for cards
-  const trash = document.querySelector('.gallery__trash');
+  //Trash, using --closest--
+  const trash = galleryElement.querySelector('.gallery__trash');
   trash.addEventListener('click', function () {
-    galleryContainer.remove();
+    const trashItem = trash.closest('.gallery__container');
+    trashItem.remove();
   });
 
-  const galleryItem = document.querySelector('.gallery__item'); //place image after add from the form
-
-  //Block function display large images
-  const popupImageLarge = document.querySelector('.popup__image-src'); // src image
-  const popupImageCaption = document.querySelector('.popup__caption'); //caption for large images
-
-  galleryItem.addEventListener('click', function () {
-    popupOpenImg();
-    popupImageLarge.src = linkImageValue;
-    popupImageLarge.alt = namePlaceValue;
-    popupImageCaption.textContent = namePlaceValue;
-  });
+  return galleryElement;
 }
 
-//Function images add - the initial array go to it and then to addPlace
-function CardAdd() {
-  initialCards.forEach(function (item) {
-    const name = item.name;
-    const link = item.link;
-    addPlace(link, name);
-  });
+// Add any item to container
+function addItem(container, item) {
+  container.prepend(item);
 }
-CardAdd(); //function call
 
-//End block gallery images
+// Init gallery
+initialCards.forEach(function (item) {
+  const name = item.name;
+  const link = item.link;
+  let newCard = createCard(link, name); // creating new card
+  addItem(gallery, newCard); // adding new card to the gallery
+});
 
-// Open the popup form adding images
-function popupOpenAdd() {
-  popupAdd.classList.add('popup_opened');
-}
-//Open a large image popup
+// Open a large image popup
 function popupOpenImg() {
   popupImages.classList.add('popup_opened');
 }
 
-// Open the profile editing popup and fill in inputs
-function popupOpenEdit() {
-  popup.classList.add('popup_opened');
-  nameInput.value = profileName.innerHTML;
-  jobInput.value = profileJob.innerHTML;
-}
+// function closing popups
 
-//Close form on clicking on the cross - list necessary popups
-function popupClose() {
-  popup.classList.remove('popup_opened');
-  popupAdd.classList.remove('popup_opened');
-  popupImages.classList.remove('popup_opened');
+function popupOpenClose() {
+  const parent = document.querySelector('.page');
+  parent.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('profile__button-edit')) {
+      popup.classList.add('popup_opened');
+      nameInput.value = profileName.innerHTML;
+      jobInput.value = profileJob.innerHTML;
+    }
+    if (event.target.classList.contains('profile__button-add')) {
+      popupAdd.classList.add('popup_opened');
+    }
+    if (evt.target.classList.contains('popup__close-toggle')) {
+      // close popup on cross (without saving data)
+      evt.target.closest('.popup').classList.remove('popup_opened');
+    }
+    if (evt.target.classList.contains('popup__item')) {
+      // select opened popup
+      let pop = document.querySelector('.popup_opened');
+      // check if any popup opened
+      if (pop != null) {
+        // select required fields on the currenct popup
+        let requiredFileds = pop.querySelectorAll('[required]');
+
+        // Go through selected required fields and check if it is empty
+        for (let i = 0; i < requiredFileds.length; i++) {
+          // if any required filed is empty then return from the function before closing popup
+          // if (requiredFileds[i].value.trim() == '') return; // without pattern (field type) validation
+          if (!requiredFileds[i].checkValidity()) return;
+        }
+      }
+
+      // close popup on save date
+      evt.target.closest('.popup').classList.remove('popup_opened');
+    }
+  });
 }
 
 // Event on the button in the profile
@@ -142,6 +131,7 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  popupOpenClose();
 }
 
 // Event on the button images editing and image add via submit
@@ -149,20 +139,20 @@ function formSubmitHandlerAdd(evt) {
   evt.preventDefault();
   let name = namePlace.value;
   let link = linkImage.value;
-  addPlace(link, name);
+  let newCard = createCard(link, name); // creating new card
+  addItem(gallery, newCard); // adding new card to the gallery
   namePlace.value = '';
   linkImage.value = '';
+  popupOpenClose();
 }
 
 // Select the proprite cross button to close
 toggle.forEach(function (item) {
-  item.addEventListener('click', popupClose);
+  item.addEventListener('click', popupOpenClose);
 });
 
 // Listeners
-submit.addEventListener('click', popupClose); //save and close in the profile form
-submitAdd.addEventListener('click', popupClose); //save and close in the profile image
-buttonEdit.addEventListener('click', popupOpenEdit); //button for opening profile editing
-buttonAdd.addEventListener('click', popupOpenAdd); //button for open add images
+buttonEdit.addEventListener('click', popupOpenClose); //button for opening profile editing
+buttonAdd.addEventListener('click', popupOpenClose); //button for open add images
 formElement.addEventListener('submit', formSubmitHandler); //sending a profile editing form
 formElementAdd.addEventListener('submit', formSubmitHandlerAdd); //sending a form adding images
