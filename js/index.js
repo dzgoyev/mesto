6ca8c85a131cfac9dafa692050efafe626a02390
dzgoyev <dzgoyev@yandex.ru>
2020-05-31 23:33:52 +0300

    Исправить функционал открытия/закрытия попапов

--- js/index.js
+++ js/index.js
@@ -1,65 +1,70 @@
 // Popups
-const popup = document.querySelector('.popup'); //popup profile (other be its elements)
-const popupAdd = document.querySelector('.popup__add'); // popup for adding a card
+const popupProfile = document.querySelector('.popup'); //popup profile (other be its elements)
+const popupAddCard = document.querySelector('.popup__add'); // popup for adding a card
 const popupImages = document.querySelector('.popup__images'); // popup for large images
 
 // buttons
-const submit = popup.querySelector('.popup__form-button'); //button in the profile form
+const submit = popupProfile.querySelector('.popup__form-button'); //button in the profile form
 const buttonEdit = document.querySelector('.profile__button-edit'); //save button in the profile form edit
 const buttonAdd = document.querySelector('.profile__button-add'); //button in the adding form
-const submitAdd = popupAdd.querySelector('.popup__item_add'); //save button in the form add images
+const submitAdd = popupAddCard.querySelector('.popup__item_add'); //save button in the form add images
 const toggle = document.querySelectorAll('.popup__close-toggle'); //close button - cross
 
 // Forms
 const formElement = document.querySelector('.popup__form'); // profile editing form
 const formElementAdd = document.querySelector('.popup__form_add'); // form for add images
 
-// Modifier
-const popupOpened = popup.querySelector('.popup_opened'); //open and close modifier popup
-
-//сюда галлерею
+//Gallery
 const container = document.querySelector('.main'); //main container
 const gallery = container.querySelector('.gallery'); //all gallery
 
-//вытащил наружу темплэйт
+//Template
 const galleryTemplate = document.querySelector('#gallery-template').content;
 
 // Inputs and output fileds profile
-const nameInput = popup.querySelector('.popup__form-item_name'); //name field in the profile registration
-const jobInput = popup.querySelector('.popup__form-item_job'); // name job in the profile registration
+const nameInput = popupProfile.querySelector('.popup__form-item_name'); //name field in the profile registration
+const jobInput = popupProfile.querySelector('.popup__form-item_job'); // name job in the profile registration
 const profileName = document.querySelector('.profile__name'); //name element on the page
 const profileJob = document.querySelector('.profile__job'); //name job jn the page
+nameInput.value = profileName.innerHTML;
+jobInput.value = profileJob.innerHTML;
 
 // Inputs and output fields add images
+const namePlace = popupAddCard.querySelector('.popup__form-item_place'); //name place in the form add
+const linkImage = popupAddCard.querySelector('.popup__form-item_link-img'); //link img in the form add
 
-const namePlace = popupAdd.querySelector('.popup__form-item_place'); //name place in the form add
-const linkImage = popupAdd.querySelector('.popup__form-item_link-img'); //link img in the form add
+// Toggle popup
+function togglePopup(popup) {
+  popup.classList.toggle('popup_opened');
+}
 
 //The function on creating cards
-
 function createCard(linkImageValue, namePlaceValue) {
   const galleryElement = galleryTemplate.cloneNode(true);
   galleryElement.querySelector('.gallery__item').src = linkImageValue;
   galleryElement.querySelector('.gallery__title').textContent = namePlaceValue;
-  galleryElement.querySelector('.gallery__item').alt = namePlaceValue;
+  galleryElement.querySelector(
+    '.gallery__item'
+  ).alt = `${namePlaceValue} - фотография`;
   // --- large image output
   const cardImage = galleryElement.querySelector('.gallery__item');
   const popupImageLarge = document.querySelector('.popup__image-src'); // src image
   const popupImageCaption = document.querySelector('.popup__caption'); //caption for large images
   cardImage.src = linkImageValue;
-  cardImage.addEventListener('click', function () {
-    popupOpenImg(); //the function adds a modifier for pop-up popup large images
+  cardImage.addEventListener('click', () => {
+    togglePopup(popupImages); //the function adds a modifier for pop-up popup large images
     popupImageLarge.src = linkImageValue;
     popupImageLarge.alt = `${namePlaceValue} - увеличенное изображение`;
     popupImageCaption.textContent = namePlaceValue;
   });
+
   //Like
   galleryElement
     .querySelector('.gallery__like')
     .addEventListener('click', function (evt) {
       evt.target.classList.toggle('gallery__like_active');
     });
-  //Trash, using --closest--
+  //Trash, using -- closest --
   const trash = galleryElement.querySelector('.gallery__trash');
   trash.addEventListener('click', function () {
     const trashItem = trash.closest('.gallery__container');
@@ -78,81 +83,48 @@ function addItem(container, item) {
 initialCards.forEach(function (item) {
   const name = item.name;
   const link = item.link;
-  let newCard = createCard(link, name); // creating new card
+  const newCard = createCard(link, name); // creating new card
   addItem(gallery, newCard); // adding new card to the gallery
 });
-
-// Open a large image popup
-function popupOpenImg() {
-  popupImages.classList.add('popup_opened');
-}
-
-// function closing popups
-
-function popupOpenClose() {
-  const parent = document.querySelector('.page');
-  parent.addEventListener('click', function (evt) {
-    if (evt.target.classList.contains('profile__button-edit')) {
-      popup.classList.add('popup_opened');
-      nameInput.value = profileName.innerHTML;
-      jobInput.value = profileJob.innerHTML;
-    }
-    if (event.target.classList.contains('profile__button-add')) {
-      popupAdd.classList.add('popup_opened');
-    }
-    if (evt.target.classList.contains('popup__close-toggle')) {
-      // close popup on cross (without saving data)
-      evt.target.closest('.popup').classList.remove('popup_opened');
-    }
-    if (evt.target.classList.contains('popup__item')) {
-      // select opened popup
-      let pop = document.querySelector('.popup_opened');
-      // check if any popup opened
-      if (pop != null) {
-        // select required fields on the currenct popup
-        let requiredFileds = pop.querySelectorAll('[required]');
-
-        // Go through selected required fields and check if it is empty
-        for (let i = 0; i < requiredFileds.length; i++) {
-          // if any required filed is empty then return from the function before closing popup
-          // if (requiredFileds[i].value.trim() == '') return; // without pattern (field type) validation
-          if (!requiredFileds[i].checkValidity()) return;
-        }
-      }
-
-      // close popup on save date
-      evt.target.closest('.popup').classList.remove('popup_opened');
-    }
-  });
-}
 
 // Event on the button in the profile
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupOpenClose();
+  togglePopup(popupProfile);
 }
 
 // Event on the button images editing and image add via submit
 function formSubmitHandlerAdd(evt) {
   evt.preventDefault();
-  let name = namePlace.value;
-  let link = linkImage.value;
-  let newCard = createCard(link, name); // creating new card
+  const name = namePlace.value;
+  const link = linkImage.value;
+  const newCard = createCard(link, name); // creating new card
   addItem(gallery, newCard); // adding new card to the gallery
   namePlace.value = '';
   linkImage.value = '';
-  popupOpenClose();
+  togglePopup(popupAddCard);
 }
 
-// Select the proprite cross button to close
-toggle.forEach(function (item) {
-  item.addEventListener('click', popupOpenClose);
-});
-
 // Listeners
-buttonEdit.addEventListener('click', popupOpenClose); //button for opening profile editing
-buttonAdd.addEventListener('click', popupOpenClose); //button for open add images
-formElement.addEventListener('submit', formSubmitHandler); //sending a profile editing form
-formElementAdd.addEventListener('submit', formSubmitHandlerAdd); //sending a form adding images
+buttonEdit.addEventListener('click', () => {
+  togglePopup(popupProfile);
+}); // Open Edit Profile
+buttonAdd.addEventListener('click', () => {
+  togglePopup(popupAddCard);
+}); // Open Add Image
+formElement.addEventListener('submit', formSubmitHandler); // submit Profile form
+formElementAdd.addEventListener('submit', formSubmitHandlerAdd); // submit Add Card form
+
+// Init close buttons listeners
+const popupCloseButtons = document.getElementsByClassName(
+  'popup__close-toggle'
+);
+if (popupCloseButtons != null) {
+  for (let i = 0; i < popupCloseButtons.length; i++) {
+    popupCloseButtons[i].addEventListener('click', () => {
+      togglePopup(document.querySelector('.popup_opened'));
+    });
+  }
+}
